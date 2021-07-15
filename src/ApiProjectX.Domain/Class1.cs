c2272dbb27bd7494ccad3c7564ec35f7e33c57a0
Marcelo Rocha <celoficial@hotmail.com>
2021-07-15 14:43:42 -0300

    arquitetura inicial

---
+++ src/ApiProjectX.Domain/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace ApiProjectX.Domain
+{
+    public class Class1
+    {
+    }
+}
---
+++ src/ApiProjectX.Service/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace ApiProjectX.Service
+{
+    public class Class1
+    {
+    }
+}
