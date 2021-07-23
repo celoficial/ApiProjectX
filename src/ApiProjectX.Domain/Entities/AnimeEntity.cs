c5c90a08e6f8f0f1c90bb06cd4957ac6202ad9dd
Sarah <sarahmenezesbraga@hotmail.com>
2021-07-23 15:08:41 -0300

    add-migration

--- src/ApiProjectX.Domain/Entities/AnimeEntity.cs
+++ src/ApiProjectX.Domain/Entities/AnimeEntity.cs
@@ -6,7 +6,7 @@ namespace ApiProjectX.Domain.Entities
     {
         public string Title { get; set; }
         public string Description { get; set; }
-        public double Rating { get; set; }
+        public double? Rating { get; set; }
         public string ImgUrl { get; set; }
 
       //  public Guid StudioId { get; set; }
---
+++ src/ApiProjectX.Infra.Data/Migrations/20210723180752_AnimeStudio.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ApiProjectX.Infra.Data.Migrations
+{
+    public partial class AnimeStudio : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Author",
+                table: "Anime");
+
+            migrationBuilder.DropColumn(
+                name: "AuthorUrl",
+                table: "Anime");
+
+            migrationBuilder.DropColumn(
+                name: "Gen",
+                table: "Anime");
+
+            migrationBuilder.AlterColumn<double>(
+                name: "Rating",
+                table: "Anime",
+                type: "float",
+                maxLength: 5,
+                nullable: true,
+                oldClrType: typeof(double),
+                oldType: "float");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Anime",
+                type: "nvarchar(255)",
+                maxLength: 255,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "Studio",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    StudioName = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: true),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Studio", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Studio");
+
+            migrationBuilder.AlterColumn<double>(
+                name: "Rating",
+                table: "Anime",
+                type: "float",
+                nullable: false,
+                defaultValue: 0.0,
+                oldClrType: typeof(double),
+                oldType: "float",
+                oldMaxLength: 5,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Anime",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(255)",
+                oldMaxLength: 255,
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Author",
+                table: "Anime",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "AuthorUrl",
+                table: "Anime",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Gen",
+                table: "Anime",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+    }
+}
