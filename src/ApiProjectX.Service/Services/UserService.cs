4855e15f3ea040337f4835aa21f7c6bd086cb2f0
Karine <karine.rs93@gmail.com>
2021-08-17 21:08:09 -0300

    User crud

---
+++ src/ApiProjectX.Service/Services/UserService.cs
@@ -0,0 +1,15 @@
+﻿using ApiProjectX.Domain.Entities;
+using ApiProjectX.Domain.Interfaces.Repository;
+using ApiProjectX.Domain.Interfaces.Services;
+
+namespace ApiProjectX.Service.Services
+{
+    class UserService : BaseService<UserEntity>, IUserService
+    {
+        private readonly IRepositoryWrapper _repository;
+        public UserService(IRepositoryWrapper repository, IBaseRepository<UserEntity> baseRepository) : base(baseRepository)
+        {
+            _repository = repository;
+        }
+    }
+}
