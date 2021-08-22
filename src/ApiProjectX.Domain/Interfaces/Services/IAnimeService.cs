99c1f04503ffece9a2a6bd93006f7e6a3a04c8dc
Marcelo Rocha <celoficial@hotmail.com>
2021-08-22 15:21:26 -0300

    Request's & Responses

--- src/ApiProjectX.Domain/Interfaces/Services/IAnimeService.cs
+++ src/ApiProjectX.Domain/Interfaces/Services/IAnimeService.cs
@@ -1,8 +1,12 @@
 ﻿using ApiProjectX.Domain.Entities;
+using ApiProjectX.Domain.Responses;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace ApiProjectX.Domain.Interfaces.Services
 {
     public interface IAnimeService : IBaseService<AnimeEntity>
     {
+        Task<GenericResult<List<AnimeAllResponse>>> GetAllResponse();
     }
 }
---
+++ src/ApiProjectX.Domain/Responses/GenericResult.cs
@@ -0,0 +1,9 @@
+﻿namespace ApiProjectX.Domain.Responses
+{
+    public class GenericResult<T>
+    {
+        public bool Success { get; set; }
+        public string? Message { get; set; }
+        public T Data { get; set; }
+    }
+}
