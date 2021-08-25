a393e0a2a28bb9cc75b9bf56ff7a022dfcc3359e
Sarah <sarahmenezesbraga@hotmail.com>
2021-08-25 17:40:59 -0300

    Criação dto StudioService

---
+++ src/ApiProjectX.Domain/Requests/Studio/Profile/StudioRequestProfile.cs
@@ -0,0 +1,13 @@
+﻿using ApiProjectX.Domain.Entities;
+using AutoMapper;
+
+namespace ApiProjectX.Domain.Requests.Studio
+{
+    public class StudioRequestProfile : Profile
+    {
+        public StudioRequestProfile()
+        {
+            CreateMap<StudioRequest, StudioEntity>();
+        }
+    }
+}
---
+++ src/ApiProjectX.Domain/Responses/Studio/Profile/StudioResponseProfile.cs
@@ -0,0 +1,15 @@
+﻿using ApiProjectX.Domain.Entities;
+using AutoMapper;
+
+namespace ApiProjectX.Domain.Responses.Studio
+{
+    public class StudioResponseProfile : Profile
+    {
+        public StudioResponseProfile()
+        {
+            CreateMap<StudioEntity, StudioAllResponse>();
+            CreateMap<StudioEntity, StudioResponse>();
+
+        }
+    }
+}
