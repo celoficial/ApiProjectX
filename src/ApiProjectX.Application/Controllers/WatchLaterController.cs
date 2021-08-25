ba6c7c8165c17380f8a195a6a628b9e28fcbd8ee
Karine <karine.rs93@gmail.com>
2021-08-25 16:17:42 -0300

    WatchLater Response/Request

--- src/ApiProjectX.Application/Controllers/WatchLaterController.cs
+++ src/ApiProjectX.Application/Controllers/WatchLaterController.cs
@@ -1,5 +1,7 @@
 ﻿using ApiProjectX.Domain.Entities;
 using ApiProjectX.Domain.Interfaces.Services;
+using ApiProjectX.Domain.Requests.WatchLater;
+using ApiProjectX.Domain.Responses;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -22,37 +24,37 @@ namespace ApiProjectX.Application.Controllers
         [HttpGet]
         public async Task<ActionResult> Get()
         {
-            IEnumerable<WatchLaterEntity> result = await _watchLaterService.GetAll();
+            var result = await _watchLaterService.GetAll();
             return Ok(result);
         }
 
         [HttpGet("id")]
         public async Task<ActionResult> Get(Guid Id)
         {
-            WatchLaterEntity result = await _watchLaterService.FindById(Id);
+            GenericResult result = await _watchLaterService.FindById(Id);
             return Ok(result);
         }
 
         [HttpPost]
-        public async Task<ActionResult> Post(WatchLaterEntity watchLaterEntity)
+        public async Task<ActionResult> Post(WatchLaterRequest watchLaterEntity)
         {
-            WatchLaterEntity result = await _watchLaterService.Create(watchLaterEntity);
+            GenericResult result = await _watchLaterService.Create(watchLaterEntity);
             return Ok(result);
         }
 
         [HttpPut]
-        public async Task<ActionResult> Put(WatchLaterEntity watchLaterEntity)
+        public async Task<ActionResult> Put(WatchLaterRequest watchLaterEntity)
         {
-            Task result = await _watchLaterService.Update(watchLaterEntity);
+            GenericResult result = await _watchLaterService.Update(watchLaterEntity);
             return Ok(result);
         }
 
         [HttpDelete]
-        public async Task<ActionResult> Delete(WatchLaterEntity watchLaterEntity)
+        public async Task<ActionResult> Delete(WatchLaterRequest watchLaterEntity)
         {
             if (!String.IsNullOrEmpty(watchLaterEntity.Id.ToString()))
             {
-                Task result = await _watchLaterService.Delete(watchLaterEntity.Id);
+                GenericResult result = await _watchLaterService.Delete(watchLaterEntity.Id);
                 return Ok(result);
             }
             else
